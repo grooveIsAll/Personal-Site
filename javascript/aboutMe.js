87d7998cf3899aacfe5d196c1e76d7e2f73483e9
Robert <robert.m.velasco@gmail.com>
2016-03-16 17:08:20 -0700

    add project files

---
+++ javascript/aboutMe.js
@@ -0,0 +1,14 @@
+$(function() {
+
+  "use strict";
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $('.profilePic').on('click', function() {
+    $('#greeting').typed({
+      strings: [" ^1500 Hello, ^500 my name is Robert! ^1500 <br>I am an <a class='typedModal' href='#artistModal' data-toggle='modal'>artist</a> and a <a class='typedModal' href='#devModal' data-toggle='modal'>Software Engineer</a> ^250 new to San Francisco. ^1500 <br> Check me out ^500 on social media ^250 and click on the links to my latest projects!"],
+      typeSpeed: 35
+    });
+  });
+  
+});
