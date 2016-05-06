6d6765d4b810d4f062b1f44b09ac81e2ced6d64b
Bobby Velasco <robert.m.velasco@gmail.com>
2016-05-06 12:31:06 -0700

    Update aboutMe.js

--- javascript/aboutMe.js
+++ javascript/aboutMe.js
@@ -6,7 +6,7 @@ $(function() {
 
   $('.profilePic').on('click', function() {
     $('#greeting').typed({
-      strings: [" ^1500 Hello, ^500 my name is Robert! ^1500 <br>I am an <a class='typedModal' href='#artistModal' data-toggle='modal'>artist</a> and a <a class='typedModal' href='#devModal' data-toggle='modal'>Software Engineer</a> ^250 new to San Francisco. ^1500 <br> Check me out ^500 on social media ^250 and click on the links to my latest projects!"],
+      strings: [" ^1500 Hello, ^500 my name is Robert! ^1500 <br>I am an <a class='typedModal' href='#artistModal' data-toggle='modal'>artist</a> and a <a class='typedModal' href='#devModal' data-toggle='modal'>Software Engineer</a> ^250. ^1500 <br> Check me out ^500 on social media ^250 and click on the links to my latest projects!"],
       typeSpeed: 35
     });
   });
